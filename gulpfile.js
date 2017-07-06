a90a486d87b51134d6198829c2a500889913a646
Vagner Santos <vagner.santos@enext.local>
2017-07-06 20:22:47 -0300

    task runner configuration

---
+++ gulpfile.js
@@ -0,0 +1,165 @@
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+/*
+    ===========================
+    BASE TASKS
+    ===========================
+*/
+var tasks = {
+    server: function(path){
+        return function(){
+            browserSync.init({
+                server: {
+                    baseDir: path
+                }
+            });
+        };
+    },
+    
+    copy: function(path, dist){
+        return function(){
+            return gulp.src(path)
+                .pipe(gulp.dest(dist));
+        }
+    },
+    
+    clean: function(path){
+        return function() {
+            return gulp.src(path).pipe(clean());
+        };
+    },
+    
+    css: function(mode, path, dist){
+        return function() {
+            var stream = gulp.src(path).pipe(sass()).on('error', utils.swallowError);
+            
+            if( mode !== 'dev' ){
+                stream = stream.pipe(cssmin());
+            }
+            
+            return stream.pipe(gulp.dest(dist)).pipe(browserSync.stream());
+        };
+    },
+    
+    js: function(mode, path, dist){
+        return function() {
+            var stream = gulp.src(path).pipe(concat('app.js'));
+                
+                if( mode !== 'dev' ){
+                    stream = stream.pipe(uglify()).on('error', utils.swallowError);
+                }
+            
+            return stream.pipe(gulp.dest(dist));
+        };
+    },
+    
+    reload: function(){
+        return function(){
+            reload();
+        }
+    },
+    
+    watch: function(){
+        return function(){
+            var html = gulp.watch('./src/*.html', function(){
+                runSequence('copy', 'reload');
+            });
+            
+            var js = gulp.watch('./src/js/**/*.js', function(){
+                runSequence('js', 'reload');
+            });
+            
+            var css = gulp.watch('./src/scss/**/*.scss', function(){
+                runSequence('css', 'reload');
+            });
+            
+            html.on('change', function(event) {
+              console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+            
+            js.on('change', function(event) {
+              console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+            
+            css.on('change', function(event) {
+              console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+        };
+    }
+};
+
+
+/*
+    ===========================
+    UTILS FUNCTIONS
+    ===========================
+*/
+var utils = {
+	swallowError: function(error) {
+		console.log(error.toString());
+		utils.alert(error.toString());
+		this.emit('end');
+	},
+
+	alert: function(msg){
+		gulp.src('./src').pipe(notify(msg));
+	}
+};
+
+
+
+/*
+    ===========================
+    GENERIC TASKS
+    ===========================
+*/
+gulp.task('reload', tasks.reload());
+gulp.task('watch', tasks.watch());
+
+
+
+/*
+    ===========================
+    DIST TASKS
+    ===========================
+*/
+gulp.task('server', tasks.server('./dist'));
+gulp.task('copy', tasks.copy('./src/*.html', './dist'));
+gulp.task('clean', tasks.clean('./dist'));
+gulp.task('css', tasks.css(['./src/scss/**/*.scss'], './dist/css'));
+gulp.task('js', tasks.js(
+    ['./src/js/libs/**/*.js', './src/js/model/**/*.js', './src/js/view/**/*.js', './src/js/control/**/*.js', './src/js/app.js'],
+    './dist/js/'
+));
+
+gulp.task('dist', function(){
+    runSequence('clean', 'copy', 'js', 'css', 'watch', 'server');
+});
+
+
+/*
+    ===========================
+    DEV TASKS
+    ===========================
+*/
+gulp.task('dev-server', tasks.server('./build'));
+gulp.task('dev-copy', tasks.copy('./src/*.html', './build'));
+gulp.task('dev-css', tasks.css('dev', ['./src/scss/**/*.scss'], './build/css'));
+gulp.task('dev-js', tasks.js(
+    'dev', 
+    ['./src/js/libs/**/*.js', './src/js/model/**/*.js', './src/js/view/**/*.js', './src/js/control/**/*.js', './src/js/app.js'],
+    './build/js/'
+));
+
+gulp.task('default', function(){
+    runSequence('clean', 'copy', 'dev-js', 'dev-css', 'watch', 'dev-server');
+});
